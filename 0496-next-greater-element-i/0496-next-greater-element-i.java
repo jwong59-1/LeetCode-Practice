b492b2d94da6742d50473a271ef512d6ad2b7af8
Justin Wong <jwong59@usfca.edu>
2026-07-18 17:58:14 -0700

    0496-NEXT-GREATER-ELEMENT-I Stats: Time: 4 ms (74.44%), Space: 45.3 MB (36.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] res = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            boolean found = false;
-            int ans = -1;
-            int target = nums1[i];
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == target) {
-                    found = true;
-                }
-                else if (found && nums2[j] > target) {
-                    ans = nums2[j];
-                    break;
-                }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        //track with hashmap and stack
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(),num);  // the number in num1 : the 2nd largest val
             }
-            res[i] = ans;
-        }    
-        return res;
+            stack.push(num);
+        }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.getOrDefault(nums1[i],-1);
+        }
+        return ans;
     }
 }
